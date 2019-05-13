a14f7e7ece49821f9dc86bf93689686e62db8e45
LiChunUcf <lichun@ucfgroup.com>
2019-05-13 18:29:46 +0800

    cli ready

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const app_config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config.json')).toString()).DEV;
+const { LISTEN_PORT = 3000, BACKEND, TITLE } = app_config;
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, '../src/index.js'),
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        inline: true,  //自动刷新
+        port: LISTEN_PORT,
+        proxy: {
+
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract({ use: ['css-loader', 'sass-loader'] })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../index.html'),
+            title: TITLE,
+            filename: 'index.html',
+            inject: true
+        })
+    ]
+}
---
+++ webpack/webpack.config.dist.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const app_config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config.json')).toString()).DIST;
+const { TITLE } = app_config;
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, '../src/App.js'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle-[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract({ use: ['css-loader', 'sass-loader'] })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader?limit=1&name=images/[hash:8].[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, '../index.html'),
+        //     title: TITLE,
+        //     filename: 'index.html',
+        //     inject: true
+        // }),
+        new ExtractTextPlugin('style-[hash].css'),
+        new webpack.DefinePlugin({
+            APP_MODE: JSON.stringify('PRODUCTION')
+        })
+    ]
+}
